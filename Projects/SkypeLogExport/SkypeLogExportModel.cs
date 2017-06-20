209d1a068937267f6c1ed16d798e1abf9bb6e332
kishimoto-exvision <kishimoto@exvision.co.jp>
2017-06-20 13:23:31 +0900

    Updating SkypeLogExport
     * Added ChatLogModel.cs

--- Projects/SkypeLogExport/SkypeLogExportModel.cs
+++ Projects/SkypeLogExport/SkypeLogExportModel.cs
@@ -5,12 +5,23 @@
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using System.ComponentModel;
     using sds = System.Data.SQLite;
 
-    public class SkypeLogExportModel
+    public class SkypeLogExportModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var t = PropertyChanged;
+            if (t != null) { t(this, new PropertyChangedEventArgs(propertyName)); }
+        }
+
         public string SkypeDataFolderPath { get; set; }
         public string SkypeMainDbFilePath { get; set; }
+        public SkypeMainDbDataSet SkypeData { get; private set; }
+        public ChatLogModel ConversionResult { get; private set; }
+
         public bool TryUpdateSkypeMainDbPath()
         {
             SkypeDataFolderPath = "";
@@ -27,15 +38,55 @@
             if (mainDbPaths.Length == 0) { return false; }
             SkypeMainDbFilePath = mainDbPaths.FirstOrDefault();
 
-            UpdateJsonDataString(SkypeMainDbFilePath);
+            SkypeData = SkypeMainDbDataSet.LoadDataFromFile(SkypeMainDbFilePath);
+            OnPropertyChanged(nameof(SkypeData));
+
+            ConvertSkypeDataToSkypeLog();
+            OnPropertyChanged(nameof(ConversionResult));
 
             return true;
         }
 
-        public string JsonDataString { get; private set; }
-        public void UpdateJsonDataString(string dataBaseFilePath)
+        public static DateTime? ConvertFromSQLiteTimestampToClrLocalDateTime(long? value)
         {
-            var data = SkypeMainDbDataSet.LoadDataFromFile(dataBaseFilePath);
+            if (value.HasValue == false) { return null; }
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 1, DateTimeKind.Utc);
+            var elapsedSeconds = TimeSpan.FromSeconds(value.Value);
+            var utcTime = epoch + elapsedSeconds;
+            return utcTime.ToLocalTime();
+        }
+
+        public void ConvertSkypeDataToSkypeLog()
+        {
+            ConversionResult = new SkypeLogExport.ChatLogModel();
+            ConversionResult.ChannelList = SkypeData.Conversations.Select(e =>
+            {
+                var newChannel = new Channel();
+                newChannel.Id = e.id;
+                newChannel.Identity = e.identity;
+                newChannel.DisplayName = e.displayname;
+                newChannel.CreationTimestamp = ConvertFromSQLiteTimestampToClrLocalDateTime(e.creation_timestamp);
+                newChannel.Creator_Person_Identity = e.creator;
+                return newChannel;
+            }).ToList();
+            var ChannelId_Channel_Dict = ConversionResult.ChannelList.ToDictionary(e => e.Id);
+            foreach (var e in SkypeData.Messages)
+            {
+                if (e.body_xml == null) { continue; }
+                if (e.convo_id.HasValue == false) { continue; }
+                var channel = ChannelId_Channel_Dict[e.convo_id.Value];
+                var newMessage = new Message();
+                newMessage.Channel_Identity = e.chatname;
+                newMessage.Id = e.id;
+                if (e.timestamp.HasValue) { newMessage.Timestamp = ConvertFromSQLiteTimestampToClrLocalDateTime(e.timestamp).Value; }
+                if (e.edited_timestamp.HasValue)
+                {
+                    newMessage.EditedBy_Person_Identity = e.edited_by;
+                    newMessage.EditedTimestamp = ConvertFromSQLiteTimestampToClrLocalDateTime(e.edited_timestamp.Value);
+                }
+                newMessage.Body = e.body_xml;
+                channel.MessageList.Add(newMessage);
+            }
         }
     }
 }
